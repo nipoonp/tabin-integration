bdba1ba50d143d0397d012d001b64a5e8641e7be
Nipoon Patel <patel_nipoon@yahoo.co.nz>
2023-03-22 02:03:36 +1300

    Remove if statement

--- src/services/importMenu/doshiiMenu.ts
+++ src/services/importMenu/doshiiMenu.ts
@@ -58,153 +58,151 @@ const convertDoshiiMenu = async (doshiiCredentials: IThirdPartyIntegrationsDoshi
     let productsData = data.products;
     let sequence = 0;
 
-    if (productsData.length > 0) {
-        productsData.map((item: any) => {
-            sequence++;
-            let category: ICategory = {
-                categoryId: item.tags.join(" ") + "-" + sequence,
-                name: item.tags.join(" "),
-                kitchenName: "",
-                description: "",
-                displaySequence: sequence,
-            };
+    productsData.map((item: any) => {
+        sequence++;
+        let category: ICategory = {
+            categoryId: item.tags.join(" ") + "-" + sequence,
+            name: item.tags.join(" "),
+            kitchenName: "",
+            description: "",
+            displaySequence: sequence,
+        };
 
-            categories.push(category);
+        categories.push(category);
 
-            let product: IProduct = {
-                productId: item.posId,
-                name: item.name,
-                kitchenName: item.alternateNames && item.alternateNames.default ? item.alternateNames.default.kitchen : "",
-                price: item.unitPrice,
-                skuCode: "",
-                totalQuantityAvailable: 0,
-                description: item.description,
-            };
+        let product: IProduct = {
+            productId: item.posId,
+            name: item.name,
+            kitchenName: item.alternateNames && item.alternateNames.default ? item.alternateNames.default.kitchen : "",
+            price: item.unitPrice,
+            skuCode: "",
+            totalQuantityAvailable: 0,
+            description: item.description,
+        };
 
-            products.push(product);
+        products.push(product);
 
-            let categoryProductLink: ICategoryProductLink = {
-                categoryId: item.tags.join(" ") + "-" + sequence,
-                productId: item.posId,
-                displaySequence: sequence,
-            };
+        let categoryProductLink: ICategoryProductLink = {
+            categoryId: item.tags.join(" ") + "-" + sequence,
+            productId: item.posId,
+            displaySequence: sequence,
+        };
 
-            categoryProductLinks.push(categoryProductLink);
+        categoryProductLinks.push(categoryProductLink);
 
-            if (item.includedItems.length > 0) {
-                let includedItems = item.includedItems;
-                includedItems.map((innerItem1: any) => {
-                    let product: IProduct = {
-                        productId: innerItem1.posId,
-                        name: innerItem1.name,
-                        kitchenName: innerItem1.alternateNames && innerItem1.alternateNames.default ? innerItem1.alternateNames.default.kitchen : "",
-                        price: innerItem1.unitPrice,
-                        skuCode: "",
-                        totalQuantityAvailable: innerItem1.quantity,
-                        description: "",
-                    };
-                    products.push(product);
+        if (item.includedItems.length > 0) {
+            let includedItems = item.includedItems;
+            includedItems.map((innerItem1: any) => {
+                let product: IProduct = {
+                    productId: innerItem1.posId,
+                    name: innerItem1.name,
+                    kitchenName: innerItem1.alternateNames && innerItem1.alternateNames.default ? innerItem1.alternateNames.default.kitchen : "",
+                    price: innerItem1.unitPrice,
+                    skuCode: "",
+                    totalQuantityAvailable: innerItem1.quantity,
+                    description: "",
+                };
+                products.push(product);
 
-                    let categoryProductLink: ICategoryProductLink = {
-                        categoryId: item.tags.join(" ") + "-" + sequence,
-                        productId: innerItem1.posId,
-                        displaySequence: sequence,
-                    };
+                let categoryProductLink: ICategoryProductLink = {
+                    categoryId: item.tags.join(" ") + "-" + sequence,
+                    productId: innerItem1.posId,
+                    displaySequence: sequence,
+                };
 
-                    categoryProductLinks.push(categoryProductLink);
+                categoryProductLinks.push(categoryProductLink);
 
-                    if (innerItem1.options.length > 0) {
-                        let innerOptions = innerItem1.options;
+                if (innerItem1.options.length > 0) {
+                    let innerOptions = innerItem1.options;
 
-                        innerOptions.map((opt: any, index: number) => {
-                            let modgroup: IModifierGroup = {
-                                modifierGroupId: opt.posId,
-                                name: opt.name,
-                                choiceDuplicate: opt.max,
-                                choiceMin: opt.min,
-                                choiceMax: opt.max,
-                            };
+                    innerOptions.map((opt: any, index: number) => {
+                        let modgroup: IModifierGroup = {
+                            modifierGroupId: opt.posId,
+                            name: opt.name,
+                            choiceDuplicate: opt.max,
+                            choiceMin: opt.min,
+                            choiceMax: opt.max,
+                        };
 
-                            modifierGroups.push(modgroup);
+                        modifierGroups.push(modgroup);
 
-                            let productModGroupLink: IProductModifierGroupLink = {
-                                productId: innerItem1.posId,
-                                modifierGroupId: opt.posId,
-                                displaySequence: index,
-                            };
+                        let productModGroupLink: IProductModifierGroupLink = {
+                            productId: innerItem1.posId,
+                            modifierGroupId: opt.posId,
+                            displaySequence: index,
+                        };
 
-                            productModifierGroupLinks.push(productModGroupLink);
-                            if (opt.variants && opt.variants.length > 0) {
-                                let variants = opt.variants;
+                        productModifierGroupLinks.push(productModGroupLink);
+                        if (opt.variants && opt.variants.length > 0) {
+                            let variants = opt.variants;
 
-                                variants.map((v: any, index2: number) => {
-                                    let modobj: IModifier = {
-                                        modifierId: v.posId,
-                                        name: v.name,
-                                        price: v.price,
-                                    };
-                                    modifiers.push(modobj);
+                            variants.map((v: any, index2: number) => {
+                                let modobj: IModifier = {
+                                    modifierId: v.posId,
+                                    name: v.name,
+                                    price: v.price,
+                                };
+                                modifiers.push(modobj);
 
-                                    let modifierGroupModifierLink: IModifierGroupModifierLink = {
-                                        modifierGroupId: opt.posId,
-                                        modifierId: v.posId,
-                                        displaySequence: index2,
-                                    };
+                                let modifierGroupModifierLink: IModifierGroupModifierLink = {
+                                    modifierGroupId: opt.posId,
+                                    modifierId: v.posId,
+                                    displaySequence: index2,
+                                };
 
-                                    modifierGroupModifierLinks.push(modifierGroupModifierLink);
-                                });
-                            }
-                        });
-                    }
-                });
-            }
+                                modifierGroupModifierLinks.push(modifierGroupModifierLink);
+                            });
+                        }
+                    });
+                }
+            });
+        }
 
-            if (item.options.length > 0) {
-                let itemOptions = item.options;
-                itemOptions.map((opt: any, index: number) => {
-                    let modgroup: IModifierGroup = {
-                        modifierGroupId: opt.posId,
-                        name: opt.name,
-                        choiceDuplicate: opt.max,
-                        choiceMin: opt.min,
-                        choiceMax: opt.max,
-                    };
+        if (item.options.length > 0) {
+            let itemOptions = item.options;
+            itemOptions.map((opt: any, index: number) => {
+                let modgroup: IModifierGroup = {
+                    modifierGroupId: opt.posId,
+                    name: opt.name,
+                    choiceDuplicate: opt.max,
+                    choiceMin: opt.min,
+                    choiceMax: opt.max,
+                };
 
-                    modifierGroups.push(modgroup);
+                modifierGroups.push(modgroup);
 
-                    let productModGroupLink: IProductModifierGroupLink = {
-                        productId: item.posId,
-                        modifierGroupId: opt.posId,
-                        displaySequence: index,
-                    };
+                let productModGroupLink: IProductModifierGroupLink = {
+                    productId: item.posId,
+                    modifierGroupId: opt.posId,
+                    displaySequence: index,
+                };
 
-                    productModifierGroupLinks.push(productModGroupLink);
+                productModifierGroupLinks.push(productModGroupLink);
 
-                    if (opt.variants && opt.variants.length > 0) {
-                        let variants = opt.variants;
+                if (opt.variants && opt.variants.length > 0) {
+                    let variants = opt.variants;
 
-                        variants.map((v: any, index2: number) => {
-                            let modobj: IModifier = {
-                                modifierId: v.posId,
-                                name: v.name,
-                                price: v.price,
-                            };
+                    variants.map((v: any, index2: number) => {
+                        let modobj: IModifier = {
+                            modifierId: v.posId,
+                            name: v.name,
+                            price: v.price,
+                        };
 
-                            modifiers.push(modobj);
+                        modifiers.push(modobj);
 
-                            let modifierGroupModifierLink: IModifierGroupModifierLink = {
-                                modifierGroupId: opt.posId,
-                                modifierId: v.posId,
-                                displaySequence: index2,
-                            };
+                        let modifierGroupModifierLink: IModifierGroupModifierLink = {
+                            modifierGroupId: opt.posId,
+                            modifierId: v.posId,
+                            displaySequence: index2,
+                        };
 
-                            modifierGroupModifierLinks.push(modifierGroupModifierLink);
-                        });
-                    }
-                });
-            }
-        });
-    }
+                        modifierGroupModifierLinks.push(modifierGroupModifierLink);
+                    });
+                }
+            });
+        }
+    });
     // console.log("categories", categories);
     // console.log("products", products);
     // console.log("categoryProductLinks", categoryProductLinks);
